eeae250fde671f3e6a7e092eba8f75b8a6503463
Huynh Minh Chu <huynhminhchu1999@gmail.com>
2021-08-11 09:14:54 +0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" { 
+  region = "us-east-1"
+}
+resource "aws_instance" "helloworld" {
+  ami = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t2.micro"
+  tags = { 
+    Name = "HelloWorld"
+  }
+}
+
+
