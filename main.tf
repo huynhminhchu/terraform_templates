5d4d7c5df7334698fe68faa2b867653cda49fb35
Huynh Minh Chu <huynhminhchu1999@gmail.com>
2021-08-11 14:30:24 +0700

    Create a web server

--- main.tf
+++ main.tf
@@ -1,12 +1,31 @@
-provider "aws" { 
+provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "helloworld" {
-  ami = "ami-0c2b8ca1dad447f8a"
-  instance_type = "t2.micro"
-  tags = { 
+  ami                    = "ami-09e67e426f25ce0d7"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
     Name = "HelloWorld"
   }
 }
-
+resource "aws_security_group" "mysg" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+output "public_ip" {
+  value       = aws_instance.helloworld.public_ip
+  description = "The public IP of the web server"
+}
 
